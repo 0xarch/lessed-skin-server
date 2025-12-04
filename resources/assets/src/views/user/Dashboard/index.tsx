c954825be913c13dfeb8bea692f3de3ef03912f4
Soloev's Cup <alanqa-ops@outlook.com>
2025-12-04 14:37:00 +0000

    style: add LiquidGlass for all cards

--- resources/assets/src/views/user/Dashboard/index.tsx
+++ resources/assets/src/views/user/Dashboard/index.tsx
@@ -93,7 +93,7 @@ const Dashboard: React.FC = () => {
   }, [])
 
   return (
-    <div className="card card-primary card-outline">
+    <div className="card LiquidGlass">
       <div className="card-header">
         <h3 className="card-title">{t('user.used.title')}</h3>
       </div>
